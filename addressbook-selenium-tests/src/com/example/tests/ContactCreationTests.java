fb58f1f7f7010b478a65c46ca0d6f3f9421b1f7b
saper.8111 <saper.8111@gmail.com>
2012-12-08 23:52:45 +0400

    Выполнено дз_2_1

--- addressbook-selenium-tests/src/com/example/tests/ContactCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/ContactCreationTests.java
@@ -3,36 +3,35 @@ package com.example.tests;
 import org.testng.annotations.Test;
 
 public class ContactCreationTests extends TestBase {
-	
+        
 
-	@Test
-	public void test1NonEmpryContactCreation() throws Exception {
-		app.openMainPage();
-		app.gotoAddNewPages();
-		ContactData contact = new ContactData();
-		contact.firstname = "firstname 1";
-		contact.lastname = "lastname 1";
-		contact.address = "address 1";
-		contact.home = "home 1";
-		contact.mobile = "mobile 1";
-		contact.work = "work 1";
-		contact.email = "email 1";
-		contact.email2 = "email2 1";
-		contact.byear = "byear 1";
-		contact.address2 = "address2 1";
-		contact.phone2 = "phone2 1";
-		app.fillContactForm(this, contact);
-		app.submitContactCreation();
-		app.returnToHomePages();
-	}
-	
-	@Test
-	public void test1EmpryContactCreation() throws Exception {
-		app.openMainPage();
-		app.gotoAddNewPages();
-		app.fillContactForm(this, new ContactData("", "", "", "", "", "", "", "", "", "", ""));
-		app.submitContactCreation();
-	    app.returnToHomePages();
-	}
-	}
-
+        @Test
+        public void test1NonEmpryContactCreation() throws Exception {
+                app.getNavigationHelper().openMainPage();
+                app.getContactHelper().gotoAddNewPages();
+                ContactData contact = new ContactData();
+                contact.firstname = "firstname 1";
+                contact.lastname = "lastname 1";
+                contact.address = "address 1";
+                contact.home = "home 1";
+                contact.mobile = "mobile 1";
+                contact.work = "work 1";
+                contact.email = "email 1";
+                contact.email2 = "email2 1";
+                contact.byear = "byear 1";
+                contact.address2 = "address2 1";
+                contact.phone2 = "phone2 1";
+                app.getContactHelper().fillContactForm(app, this, contact);
+                app.getContactHelper().submitContactCreation();
+                app.getContactHelper().returnToHomePages();
+        }
+        
+        @Test
+        public void test1EmpryContactCreation() throws Exception {
+                app.getNavigationHelper().openMainPage();
+                app.getContactHelper().gotoAddNewPages();
+                app.getContactHelper().fillContactForm(app, this, new ContactData("", "", "", "", "", "", "", "", "", "", ""));
+                app.getContactHelper().submitContactCreation();
+                app.getContactHelper().returnToHomePages();
+        }
+        }
--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -7,25 +7,25 @@ import org.testng.annotations.Test;
 public class GroupCreationTests extends TestBase {
 	@Test
 	public void testEmtpyGroupCreation() throws Exception {
-		app.openMainPage();
-		app.gotoGroupsPage();
-		app.initGroupCreation();
+		app.getNavigationHelper().openMainPage();
+		app.getNavigationHelper().gotoGroupsPage();
+		app.getGroupHelper().initGroupCreation();
 		GroupData group = new GroupData();
 		group.name = "name 1";
 		group.header = "header 1";
 		group.footer = "footer 1";
-		app.fillGroupsForm(this, group);
-		app.submitGroupCreation();
-		app.returnToGroupsPage();
+		app.getGroupHelper().fillGroupForm(group);
+		app.getGroupHelper().submitGroupCreation();
+		app.getGroupHelper().returnToGroupsPage();
 	}
 	
 	@Test
 	public void testNonEmtpyGroupCreation() throws Exception {
-		app.openMainPage();
-		app.gotoGroupsPage();
-		app.initGroupCreation();
-		app.fillGroupsForm(this, new GroupData("", "", ""));
-		app.submitGroupCreation();
-		app.returnToGroupsPage();
+		app.getNavigationHelper().openMainPage();
+		app.getNavigationHelper().gotoGroupsPage();
+		app.getGroupHelper().initGroupCreation();
+		app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
+		app.getGroupHelper().submitGroupCreation();
+		app.getGroupHelper().returnToGroupsPage();
 	}
 }
