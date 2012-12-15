82eb8931b540c5238d8677af7a922f92309c063e
saper.8111 <saper.8111@gmail.com>
2012-12-15 21:47:19 +0400

    Создан ContactCreationTests

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -2,19 +2,22 @@ package com.example.tests;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class GroupCreationTests {
+	
 	private WebDriver driver;
 	private String baseUrl;
 	private StringBuffer verificationErrors = new StringBuffer();
 	
-	@Before
+	@BeforeClass
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
 		baseUrl = "http://localhost/";
@@ -22,21 +25,57 @@ public class GroupCreationTests {
 	}
 
 	@Test
-	public void testUntitled() throws Exception {
-		driver.get(baseUrl + "/addressbookv4.1.4/");
-		driver.findElement(By.linkText("groups")).click();
-		driver.findElement(By.name("new")).click();
-		driver.findElement(By.name("group_name")).clear();
-		driver.findElement(By.name("group_name")).sendKeys("group 1");
-		driver.findElement(By.name("group_header")).clear();
-		driver.findElement(By.name("group_header")).sendKeys("header 1");
-		driver.findElement(By.name("group_footer")).clear();
-		driver.findElement(By.name("group_footer")).sendKeys("footer 1");
-		driver.findElement(By.name("submit")).click();
+	public void testNonEmptyGroupCreation() throws Exception {
+		openMainPage();
+		gotoGroupsPage();
+		initGroupCreation();
+		GroupData group = new GroupData();
+		group.name = "group name 1";
+		group.header = "header 1";
+		group.footer = "footer 1";
+		fillGroupForm(group);
+		submitGroupCreation();
+		returnToGroupsPage();
+	}
+	@Test
+	public void testEmptyGroupCreation() throws Exception {
+		openMainPage();
+		gotoGroupsPage();
+		initGroupCreation();
+		fillGroupForm(new GroupData("", "", ""));
+		submitGroupCreation();
+		returnToGroupsPage();
+	}
+	private void returnToGroupsPage() {
 		driver.findElement(By.linkText("group page")).click();
 	}
 
-	@After
+	private void submitGroupCreation() {
+		driver.findElement(By.name("submit")).click();
+	}
+
+	private void fillGroupForm(GroupData group) {
+		driver.findElement(By.name("group_name")).clear();
+		driver.findElement(By.name("group_name")).sendKeys(group.name);
+		driver.findElement(By.name("group_header")).clear();
+		driver.findElement(By.name("group_header")).sendKeys(group.header);
+		driver.findElement(By.name("group_footer")).clear();
+		driver.findElement(By.name("group_footer")).sendKeys(group.footer);
+	}
+
+	private void initGroupCreation() {
+		driver.findElement(By.name("new")).click();
+	}
+
+	private void gotoGroupsPage() {
+		driver.findElement(By.linkText("groups")).click();
+	}
+
+	private void openMainPage() {
+		driver.get(baseUrl + "/addressbookv4.1.4/");
+	}
+
+	@AfterClass
 	public void tearDown() throws Exception {
 		driver.quit();
 		String verificationErrorString = verificationErrors.toString();
