151af72eff758519b0caa4c4f648d3dd20028947
saper.8111 <saper.8111@gmail.com>
2012-12-15 14:51:39 +0400

    Выполнение ДЗ1 сначала

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,31 +1,56 @@
 package com.example.tests;
 
-import org.testng.annotations.Test;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-
-
-public class GroupCreationTests extends TestBase {
-	@Test
-	public void testEmtpyGroupCreation() throws Exception {
-		app.getNavigationHelper().openMainPage();
-		app.getNavigationHelper().gotoGroupsPage();
-		app.getGroupHelper().initGroupCreation();
-		GroupData group = new GroupData();
-		group.name = "name 1";
-		group.header = "header 1";
-		group.footer = "footer 1";
-		app.getGroupHelper().fillGroupForm(group);
-		app.getGroupHelper().submitGroupCreation();
-		app.getGroupHelper().returnToGroupsPage();
-	}
+public class GroupCreationTests {
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
 	
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "http://localhost/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
 	@Test
-	public void testNonEmtpyGroupCreation() throws Exception {
-		app.getNavigationHelper().openMainPage();
-		app.getNavigationHelper().gotoGroupsPage();
-		app.getGroupHelper().initGroupCreation();
-		app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
-		app.getGroupHelper().submitGroupCreation();
-		app.getGroupHelper().returnToGroupsPage();
+	public void testUntitled() throws Exception {
+		driver.get(baseUrl + "/addressbookv4.1.4/");
+		driver.findElement(By.linkText("groups")).click();
+		driver.findElement(By.name("new")).click();
+		driver.findElement(By.name("group_name")).clear();
+		driver.findElement(By.name("group_name")).sendKeys("group 1");
+		driver.findElement(By.name("group_header")).clear();
+		driver.findElement(By.name("group_header")).sendKeys("header 1");
+		driver.findElement(By.name("group_footer")).clear();
+		driver.findElement(By.name("group_footer")).sendKeys("footer 1");
+		driver.findElement(By.name("submit")).click();
+		driver.findElement(By.linkText("group page")).click();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
 	}
 }
