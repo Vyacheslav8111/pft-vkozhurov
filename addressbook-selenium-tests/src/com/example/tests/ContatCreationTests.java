46f3c749e7b548bdb0093398d77848a597bbf62f
saper.8111 <saper.8111@gmail.com>
2012-12-16 17:35:05 +0400

    тесты по созданию контакта прошли успешно

--- addressbook-selenium-tests/src/com/example/tests/ContatCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/ContatCreationTests.java
@@ -1,19 +1,23 @@
 package com.example.tests;
 
-import java.util.regex.Pattern;
+import static org.junit.Assert.fail;
+
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class ContatCreationTests {
 	private WebDriver driver;
 	private String baseUrl;
 	private StringBuffer verificationErrors = new StringBuffer();
-	@Before
+	
+	@BeforeClass
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
 		baseUrl = "http://localhost/";
@@ -21,39 +25,82 @@ public class ContatCreationTests {
 	}
 
 	@Test
-	public void testUntitled() throws Exception {
-		driver.get(baseUrl + "/addressbookv4.1.4/");
-		driver.findElement(By.linkText("home")).click();
-		driver.findElement(By.linkText("add new")).click();
+	public void testNonEmptyContactCreation() throws Exception {
+		openMainPage();
+		openHomePage();
+		initNewGroupCreation();
+		ContactData contact = new ContactData();
+		contact.firstname = "firstname 1";
+		contact.lastname = "lastname 1";
+		contact.address = "address 1";
+		contact.hometelephone = "hometelephone 1";
+		contact.mobiletelephone = "mobiletelephone 1";
+		contact.worktelephone = "worktelephone 1";
+		contact.email = "email 1";
+		contact.email_2 = "email_2 1";
+		contact.birthyear = "birthyear 1";
+		contact.secondary_address = "secondary_address 1";
+		contact.secondary_phone = "secondary_phone";
+		submitContactCreation();
+		gotoHomePage();
+	}
+
+	@Test
+	public void testEmptyContactCreation() throws Exception {
+		openMainPage();
+		initNewGroupCreation();
+		fillContactForm(new ContactData("", "", "", "", "", "", "", "", "", "", "", "", ""));
+		submitContactCreation();
+		gotoHomePage();
+	}
+	private void fillContactForm(ContactData contact) {
 		driver.findElement(By.name("firstname")).clear();
-		driver.findElement(By.name("firstname")).sendKeys("abc");
+		driver.findElement(By.name("firstname")).sendKeys(contact.firstname);
 		driver.findElement(By.name("lastname")).clear();
-		driver.findElement(By.name("lastname")).sendKeys("ABC");
+		driver.findElement(By.name("lastname")).sendKeys(contact.lastname);
 		driver.findElement(By.name("address")).clear();
-		driver.findElement(By.name("address")).sendKeys("street Soviet");
+		driver.findElement(By.name("address")).sendKeys(contact.address);
 		driver.findElement(By.name("home")).clear();
-		driver.findElement(By.name("home")).sendKeys("house");
+		driver.findElement(By.name("home")).sendKeys(contact.hometelephone);
 		driver.findElement(By.name("mobile")).clear();
-		driver.findElement(By.name("mobile")).sendKeys("332332");
+		driver.findElement(By.name("mobile")).sendKeys(contact.mobiletelephone);
 		driver.findElement(By.name("work")).clear();
-		driver.findElement(By.name("work")).sendKeys("878756");
+		driver.findElement(By.name("work")).sendKeys(contact.worktelephone);
 		driver.findElement(By.name("email")).clear();
-		driver.findElement(By.name("email")).sendKeys("door@yandex.ru");
+		driver.findElement(By.name("email")).sendKeys(contact.email);
 		driver.findElement(By.name("email2")).clear();
-		driver.findElement(By.name("email2")).sendKeys("open@yandex.ru");
+		driver.findElement(By.name("email2")).sendKeys(contact.email_2);
 		new Select(driver.findElement(By.name("bday"))).selectByVisibleText("1");
 		new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText("January");
 		driver.findElement(By.name("byear")).clear();
-		driver.findElement(By.name("byear")).sendKeys("2001");
+		driver.findElement(By.name("byear")).sendKeys(contact.birthyear);
 		driver.findElement(By.name("address2")).clear();
-		driver.findElement(By.name("address2")).sendKeys("Soviet street");
+		driver.findElement(By.name("address2")).sendKeys(contact.secondary_address);
 		driver.findElement(By.name("phone2")).clear();
-		driver.findElement(By.name("phone2")).sendKeys("dorf");
-		driver.findElement(By.name("submit")).click();
+		driver.findElement(By.name("phone2")).sendKeys(contact.secondary_phone);
+	}
+
+	private void gotoHomePage() {
 		driver.findElement(By.linkText("home page")).click();
 	}
 
-	@After
+	private void submitContactCreation() {
+		driver.findElement(By.name("submit")).click();
+	}
+
+	private void initNewGroupCreation() {
+		driver.findElement(By.linkText("add new")).click();
+	}
+
+	private void openHomePage() {
+		driver.findElement(By.linkText("home")).click();
+	}
+
+	private void openMainPage() {
+		driver.get(baseUrl + "/addressbookv4.1.4/");
+	}
+
+	@AfterClass
 	public void tearDown() throws Exception {
 		driver.quit();
 		String verificationErrorString = verificationErrors.toString();
